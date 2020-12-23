71c9d0ccb4aa2346e22ede90bdaf61d4fdbecf22
Pavel563 <pasha.shalimoff169@gmail.com>
2020-12-23 02:51:24 +0200

    Homework7.py

--- Homework7.py
+++ Homework7.py
@@ -49,30 +49,20 @@ my_print(my_str)
 # в) Посчитать среднее количество лет всех людей из списка.
 
 persons = [{"name": "John", "age": 45}, {"name": "Michel", "age": 50}, {"name": "Jack", "age": 15}]
-count = 0
-sum_age = 0
-min_age = 100
-len_name = 0
+ages = []
+names = []
 younger = []
 long_name = []
 for person in persons:
-    if person["age"] < min_age:
-        min_age = person["age"]
-    if len(person["name"]) > len_name:
-        len_name = len(person["name"])
-    sum_age += person["age"]
-    count += 1
-
+    ages.append(person["age"])
+    names.append(len(person["name"]))
 for person in persons:
-    if min_age == person["age"]:
+    if min(ages) == person["age"]:
         younger.append(person["name"])
-younger = ', '.join(younger)
-
-for person in persons:
-    if len_name == len(person["name"]):
+    if max(names) == len(person["name"]):
         long_name.append(person["name"])
-long_name = ', '.join(long_name)
-result = sum_age // count
+
+result = sum(ages) // len(ages)
 print(f"Самый молодой: {younger}")
 print(f"Самое длиное имя: {long_name}")
 print(f"Средний возраст: {result}")
@@ -88,20 +78,47 @@ print(f"Средний возраст: {result}")
 
 my_dict_1 = {"name": "John", "age": 25, "job": "Developer"}
 my_dict_2 = {"name": "Michel", "age": 30}
-list_a = []
-list_b = []
-dict_c = {}
-new_list = []
-new_dict = {}
-for key, value in my_dict_1.items():
-    if key in my_dict_2:
-        list_a.append(key)
-        new_dict[key] = [value, my_dict_2[key]]
-    if key not in my_dict_2:
-        list_b.append(key)
-        dict_c[key] = value
-        new_dict[key] = value
-print(list_a)
-print(list_b)
-print(dict_c)
-print(new_dict)
+keys = []
+my_dict_4 = {}
+
+keys_1 = set(my_dict_1.keys())
+keys_2 = set(my_dict_2.keys())
+
+for key in keys_1.union(keys_2):
+    if key in keys_1 and key in keys_2:
+        keys.append(key)
+print(keys)
+
+keys_1.difference_update(keys_2)
+dif_keys = list(keys_1)
+print(dif_keys)
+
+my_dict_3 = dict((key, my_dict_1[key]) for key in my_dict_1 if key not in my_dict_2)
+print(my_dict_3)
+
+for key in keys_1.union(keys_2):
+    if key in my_dict_1 and key in my_dict_2:
+        my_dict_4[key] = [my_dict_1[key], my_dict_2[key]]
+    elif key in my_dict_1 and key not in my_dict_2:
+        my_dict_4[key] = my_dict_1[key]
+    else:
+        my_dict_4[key] = my_dict_2[key]
+print(my_dict_4)
+
+# list_a = []
+# list_b = []
+# dict_c = {}
+# new_list = []
+# new_dict = {}
+# for key, value in my_dict_1.items():
+#     if key in my_dict_2:
+#         list_a.append(key)
+#         new_dict[key] = [value, my_dict_2[key]]
+#     if key not in my_dict_2:
+#         list_b.append(key)
+#         dict_c[key] = value
+#         new_dict[key] = value
+# print(list_a)
+# print(list_b)
+# print(dict_c)
+# print(new_dict)
