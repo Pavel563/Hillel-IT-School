32bf91a01efd76cc4388d1c30fd445a070d45d09
Pavel563 <pasha.shalimoff169@gmail.com>
2021-01-15 15:24:36 +0200

    Homework12.py

--- Homework12.py
+++ Homework12.py
@@ -1,3 +1,9 @@
+import requests
+import csv
+import re
+import json
+
+
 # Все пункты сделать как отдельные функции(можно создавать дополнительные вспомагательные функции)
 #
 # 1. Написать функцию, которая принимает в виде параметра целое число - количество цитат (см. урок 12).
@@ -7,53 +13,50 @@
 # Author, Quote, URL. Если автор не указан, цитату не брать.
 # Перед сохранением в csv, записи отсортировать по автору (в алфавитном порядке).
 
-# import requests
-#
-# import csv
-#
-#
-# def get_quotes(count):
-#     quote_dict_list = []
-#     i = 0
-#     url = "http://api.forismatic.com/api/1.0/"
-#
-#     params = {"method": "getQuote",
-#               "format": "json",
-#               "key": 1,
-#               "lang": "ru"}
-#     while i != count:
-#         params["key"] = i
-#         r = requests.get(url, params=params)
-#         quote = r.json()
-#         quote_dict = {"Author": "", "Quote": "", "URL": ""}
-#         if len(quote["quoteAuthor"]) > 0:
-#             quote_dict["Author"] = quote["quoteAuthor"]
-#             quote_dict["Quote"] = quote["quoteText"]
-#             quote_dict["URL"] = quote["quoteLink"]
-#             quote_dict_list.append(quote_dict)
-#             i += 1
-#     return quote_dict_list
-#
-#
-# quotes = get_quotes(10)
-#
-#
-# def key_sorted_by_name(quote):
-#     return quote["Author"]
-#
-#
-# quotes = sorted(quotes, key=key_sorted_by_name)
-#
-#
-# def write_quotes_csv(filename="quotes.csv"):
-#     with open(filename, "w", encoding="utf-8") as csv_file:
-#         fieldnames = {"Author": "", "Quote": "", "URL": ""}
-#         writer = csv.DictWriter(csv_file, fieldnames=fieldnames, delimiter=',')
-#         writer.writeheader()
-#         writer.writerows(quotes)
-#
-#
-# write_quotes_csv()
+
+def get_quotes(count):
+    quote_dict_list = []
+    i = 0
+    url = "http://api.forismatic.com/api/1.0/"
+
+    params = {"method": "getQuote",
+              "format": "json",
+              "key": 1,
+              "lang": "ru"}
+    while i != count:
+        params["key"] = i
+        r = requests.get(url, params=params)
+        quote = r.json()
+        quote_dict = {"Author": "", "Quote": "", "URL": ""}
+        if len(quote["quoteAuthor"]) > 0:
+            quote_dict["Author"] = quote["quoteAuthor"]
+            quote_dict["Quote"] = quote["quoteText"]
+            quote_dict["URL"] = quote["quoteLink"]
+            quote_dict_list.append(quote_dict)
+            i += 1
+    return quote_dict_list
+
+
+quotes = get_quotes(10)
+
+
+def key_sorted_by_name(quote):
+    return quote["Author"]
+
+
+quotes = sorted(quotes, key=key_sorted_by_name)
+
+
+def write_quotes_csv(filename="quotes.csv"):
+    with open(filename, "w", encoding="utf-8") as csv_file:
+        fieldnames = {"Author": "", "Quote": "", "URL": ""}
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames, delimiter=',')
+        writer.writeheader()
+        writer.writerows(quotes)
+
+
+write_quotes_csv()
+
 
 ########################################################################################################################
 # 2. Дан файл authors.txt
@@ -72,8 +75,9 @@ def read_txt_file(filename):
 
 
 authors = read_txt_file("authors.txt")
-print(authors)
 
+
+########################################################################################################################
 # 2.2) Написать функцию, которая принимает список строк полученной в пункте 2.1, и возвращает список словарей
 # в формате {"name": name, "date": date},
 # где name это имя автора, а date - дата из строки в формате "dd/mm/yyyy" (d-день, m-месяц, y-год)
@@ -81,41 +85,32 @@ print(authors)
 # Например [{"name": "Charles Dickens", "date": "09/06/1870"}, ...,
 # {"name": "J. D. Salinger", "date": "01/01/1919"}]
 
-import re
 
-
-def make_dict():
+def make_dict(authors):
     authors_dict_list = []
+    month_names = {
+        'January': "01",
+        'February': "02",
+        'March': "03",
+        'April': "04",
+        'May': "05",
+        'June': "06",
+        'July': "07",
+        'August': "08",
+        'September': "09",
+        'October': "10",
+        'November': "11",
+        'December': "12"
+    }
     for text in authors:
         name = re.findall(r"[a-zA-Z. ]+[']", text)
         name = [i.strip(' \'') for i in name]
         name = "".join(name)
         numbers = re.findall(r'[0-9]+', text)
         dd = numbers[0] if len(numbers[0]) > 1 else '0' + numbers[0]
-        if 'January' in text:
-            mm = '01'
-        elif 'February' in text:
-            mm = '02'
-        elif 'March' in text:
-            mm = '03'
-        elif 'April' in text:
-            mm = '04'
-        elif 'May' in text:
-            mm = '05'
-        elif 'June' in text:
-            mm = '06'
-        elif 'July' in text:
-            mm = '07'
-        elif 'August' in text:
-            mm = '08'
-        elif 'September' in text:
-            mm = '09'
-        elif 'October' in text:
-            mm = '10'
-        elif 'November' in text:
-            mm = '11'
-        elif 'December' in text:
-            mm = '12'
+        month = re.findall(r"[A-Z][a-z]+[ ][1]", text)
+        month_name = ''.join([i.strip(' 1') for i in month])
+        mm = month_names[month_name]
         yyyy = numbers[-1]
         date = f"{dd}/{mm}/{yyyy}"
         author_dict = {"name": name, "date": date}
@@ -123,7 +118,13 @@ def make_dict():
     return authors_dict_list
 
 
-authors_list = make_dict()
-print(authors_list)
-
+########################################################################################################################
 # 2.3) Написать функцию, которая сохраняет результат пункта 2.2 в json файл.
+
+
+def write_authors_json(filename="authors.json"):
+    with open(filename, "w") as json_file:
+        json.dump(make_dict(authors), json_file, indent=2)
+
+
+write_authors_json()
